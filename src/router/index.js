e80b4bdb2d31e0bfe10f85ca4d5e65d9d64b451b
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-10-24 22:18:59 -0500

    Config routes and change Views configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routerOptions = [
+  { path: '/', name: 'Home' },
+  { path: '/about', name: 'About' },
+  { path: '*', redirect: { name: 'Home' } }
 ]
+
+const routes = routerOptions.map(r => ({
+  ...r,
+  component: () => import(/* webpackChunkName: "[request]" */`@/views/${r.name}/`)
+}))
 
 const router = new VueRouter({
   mode: 'history',
