ae5b71922e8ae975816a7197ad15c086f087f48f
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-10-24 21:34:48 -0500

    Layouts

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header />
+    <div class="container">
+      <router-view />
+    </div>
+    <Footer>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header/'
+import Footer from '@/components/Footer/'
+export default {
+  name: 'MainLayout',
+  components: { Header, Footer }
+}
+</script>
