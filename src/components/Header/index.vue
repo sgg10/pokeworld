4116684e00f494e5634ca0c9764892797dd3f324
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-10-25 15:24:39 -0500

    Some changes

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -6,11 +6,6 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
-        </b-navbar-nav>
-
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-item-dropdown right v-if="user">
